8f10c6974d31fcff546e8a4194dee9cb5b9a3f57
Abdullah Alqahtani <0xqahta0@gmail.com>
2024-12-27 22:29:40 +0300

    style(utils.go): format SafeString function for consistent indentation and readability

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -7,11 +7,11 @@ import "fmt"
 // ###############################################################################
 
 func SafeString(v interface{}) string {
-    if str, ok := v.(string); ok {
-        return str
-    }
-    if num, ok := v.(float64); ok {
-        return fmt.Sprintf("%.2f", num)
-    }
-    return ""
+	if str, ok := v.(string); ok {
+		return str
+	}
+	if num, ok := v.(float64); ok {
+		return fmt.Sprintf("%.2f", num)
+	}
+	return ""
 }
